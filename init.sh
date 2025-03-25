6142f0164608d8bf8cd97e7ef784c3a4e6dec08a
mingxuan <9281463+yabostone@users.noreply.github.com>
2025-03-25 10:17:46 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 # 安装 BCC
-sudo apt install bpfcc-tools linux-headers-$(uname -r)
+sudo apt install bpfcc-tools linux-headers-$(uname -r) -y
 
 # 安装 bpftrace
-sudo apt install bpftrace
+sudo apt install bpftrace -y
 
 # 安装 LLVM 工具链
-sudo apt install clang llvm libelf-dev
+sudo apt install clang llvm libelf-dev  -y
